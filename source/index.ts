8b62a5ba3a3e88e1a75802746e6f8e7a39b2eea6
Farhan <mzulfarhan94@gmail.com>
2022-05-18 19:40:29 +0800

    cors

--- source/index.ts
+++ source/index.ts
@@ -1,7 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import initialTransactions from "./data/initialTransactions";
 import bodyParser from "body-parser";
-import cors from 'cors';
+import cors from "cors";
 
 const app: Express = express();
 const port = 3000;
